edc1995dd86f94c710cbe5c64647ea13ad06e263
yarovoi.p.98 <yarovoi.p.98@gmail.com>
2021-10-26 19:30:45 +0700

    Генерация первого числа при запуске приложения

--- RightOnTarget/ViewController.swift
+++ RightOnTarget/ViewController.swift
@@ -11,49 +11,44 @@ class ViewController: UIViewController {
     var hiddenNumber: Int = 0
     var roundNumber: Int = 0
     var score: Int = 0
-
+    
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
     
     @IBAction func checkNumber() {
-        if self.roundNumber == 0 {
-            self.hiddenNumber = Int.random(in: 0...50)
-            self.label.text = String(self.hiddenNumber)
-            self.roundNumber = 1
+        let numberOnSlider = Int(self.slider.value.rounded())
+        
+        if numberOnSlider > self.hiddenNumber {
+            self.score += 50 - self.hiddenNumber + numberOnSlider
+        } else if numberOnSlider < self.hiddenNumber {
+            self.score += 50 - numberOnSlider + self.hiddenNumber
         } else {
-            let numberOnSlider = Int(self.slider.value.rounded())
-            if numberOnSlider > self.hiddenNumber {
-                self.score += 50 - self.hiddenNumber + numberOnSlider
-            } else if numberOnSlider < self.hiddenNumber {
-                self.score += 50 - numberOnSlider + self.hiddenNumber
-            } else {
-                self.score += 50
-            }
-            if self.roundNumber == 5 {
-                let alert = UIAlertController(
-                    title: "Игра окончена.",
-                    message: "Вы заработали \(self.score) очков.",
-                    preferredStyle: .alert)
-                alert.addAction(UIAlertAction(
-                    title: "Начать заново.",
-                    style: .default,
-                    handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                self.roundNumber = 1
-                self.score = 0
-            } else {
-                self.roundNumber += 1
-            }
-            self.hiddenNumber = Int.random(in: 1...50)
-            self.label.text = String(self.hiddenNumber)
+            self.score += 50
         }
+        if self.roundNumber != 5 {
+            self.roundNumber += 1
+        } else {
+            let alert = UIAlertController(
+                title: "Игра окончена.",
+                message: "Вы заработали \(self.score) очков.",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(
+                title: "Начать заново.",
+                style: .default,
+                handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.roundNumber = 1
+            self.score = 0
+        }
+        
+        self.hiddenNumber = Int.random(in: 1...50)
+        self.label.text = String(self.hiddenNumber)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.hiddenNumber = Int.random(in: 0...50)
+        self.label.text = String(self.hiddenNumber)
     }
-
-
 }
-
